aff283b1ff3506d39acf6cccb6185a1417222510
Jordy Henry <jordy.hpereira@gmail.com>
2018-12-04 23:51:17 -0200

    color shader

--- Assets/Procedural Landmass Generation/Scripts/Data/TextureData.cs
+++ Assets/Procedural Landmass Generation/Scripts/Data/TextureData.cs
@@ -1,10 +1,31 @@
 ﻿using System.Collections;
 using UnityEngine;
 
+[CreateAssetMenu()]
 public class TextureData : UpdatableData
 {
+    public Color[] baseColors;
+    [Range(0,1)]
+    public float[] baseStartHeights;
+
+    float savedMinHeight;
+    float savedMaxHeight;
+
     public void ApplyToMaterial(Material material)
     {
+        material.SetInt("baseColorCount", baseColors.Length);
+        material.SetColorArray("baseColors", baseColors);
+        material.SetFloatArray("baseStartHeights", baseStartHeights);
 
+        UpdateMeshHeights(material, savedMinHeight, savedMaxHeight);
+    }
+
+    public void UpdateMeshHeights(Material _material, float minHeight, float maxHeight)
+    {
+        savedMaxHeight = maxHeight;
+        savedMinHeight = minHeight;
+
+        _material.SetFloat("minHeight", minHeight);
+        _material.SetFloat("maxHeight", maxHeight);
     }
 }
--- Assets/Procedural Landmass Generation/Scripts/MapGenerator.cs
+++ Assets/Procedural Landmass Generation/Scripts/MapGenerator.cs
@@ -86,6 +86,8 @@ public class MapGenerator : MonoBehaviour
             }
         }
 
+        textureData.UpdateMeshHeights(terrainMaterial, terrainData.minHeight, terrainData.maxHeight);
+
 		return new MapData(noiseMap);
 	}
 
